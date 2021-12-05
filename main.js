3d8b282f11a2dbb6c650f480797aacc1dae90a64
Kyungbok-Student <95460521+Kyungbok-Student@users.noreply.github.com>
2021-12-05 19:29:11 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(function(){
 
     $('#fullpage').fullpage({
-		//options here
+		
 		anchors:['section1', 'section2', 'section3', 'section4', 'section5'] ,
 
 		autoScrolling : true,
@@ -10,11 +10,11 @@ $(function(){
 		navigationPosition :'right', 
 		sectionsColor : [ '#000', '#000' ,'#000' ,'#000' ,'#000' ] ,
 		keyboardScrolling : true ,
-		navigationTooltips: [ 'ff','dd','dd','dd','as']
-
+		navigationTooltips: [ 'ff','dd','dd','dd','as'],
+		loopTop: true,
+		loopBottom: true ,
+		slidesNavigation: true
 		
-
-
 	});
     
 });
